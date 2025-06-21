d2aaa80edb24f8c360e5b357e7e2082dd6cf6efd
ChocolateKest <gustavoemartins@gmail.com>
2025-06-21 19:41:21 -0300

    fix code

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -3,7 +3,6 @@ package principal;
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-
         double a = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor de 0-100:"));
         if (a < 0.0 || a > 100) {
             JOptionPane.showMessageDialog(null,"Fora do intervalo");
@@ -13,9 +12,5 @@ public class Main {
             JOptionPane.showMessageDialog(null,"Intervalo [25-50]");
         } else if(a <= 75) JOptionPane.showMessageDialog(null, "Intervalo [50-75]");
         else if (a <= 100) JOptionPane.showMessageDialog(null,"Intervalo [0-100]");
-
-
-
-
     }
 }
