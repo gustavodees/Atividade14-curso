b0316478d2c7aceb4cf5418ede62cc90623eb6ce
ChocolateKest <gustavoemartins@gmail.com>
2025-06-21 19:40:30 -0300

    fix code

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -7,17 +7,16 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        int a = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor de 0-100:"));
-        if (a <= 25){
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor de 0-100:"));
+        if (a < 0.0 || a > 100) {
+            JOptionPane.showMessageDialog(null,"Fora do intervalo");
+        }else if (a <= 25){
             JOptionPane.showMessageDialog(null,"Intervalo [0-25]");
         }else if (a <= 50){
             JOptionPane.showMessageDialog(null,"Intervalo [25-50]");
-        }else if (a <= 100){
-            JOptionPane.showMessageDialog(null,"Intervalo [50-100]");
-        }
-        else{
-            JOptionPane.showMessageDialog(null,"Fora do intervalo");
-        }
+        } else if(a <= 75) JOptionPane.showMessageDialog(null, "Intervalo [50-75]");
+        else if (a <= 100) JOptionPane.showMessageDialog(null,"Intervalo [0-100]");
+
 
 
 
